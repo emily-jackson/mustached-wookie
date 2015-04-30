192053413a02a429314cb5f908018c04c73ee1b3
Brian Steele <brian.steele@brsteele-ltm2.internal.salesforce.com>
2015-04-30 14:24:24 -0400

    added regex to prevent malformed fields starting with http to create links. Also added some simple CSS and a fancy JIRA icon.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,30 +1,32 @@
+var badLinkCheck = /^(http)/ig;
 var cce = '';
 var cceArray = [];
+var tempCCE = '';
 var pdt = '';
 var pdtArray = [];
-var tempCCE = '';
 var tempPDT = '';
+
 (function checkForCase() {
     // make sure a case is loaded
     if (document.querySelector('.a-case-tab')) {
-        //  how I'm handling Desk tabs - place all CCEs into an array
+        // there can be multiple case tabs open, so place all CCEs into an array... every check... Maybe not the most efficient way to handle...
         cceArray = document.querySelectorAll('#ticket_custom21');
         cce = cceArray[cceArray.length -1].value;
     
-        // don't add a link to the page if we're still on the same page
-        if (cce !== tempCCE) {
-            cceArray[cceArray.length - 1].insertAdjacentHTML('beforebegin', "<a href='https://jira.dev.pardot.com/browse/" + cce + "' target='_blank'>" + cce + "</a>");
+        // don't add a link to the page once it's been added
+        if (cce !== tempCCE && !(badLinkCheck.test(cce))) {
+            cceArray[cceArray.length - 1].parentElement.insertAdjacentHTML('afterend', "<div class='jiraLink'><img src='https://i.imgur.com/cXxqLDL.png'><a href='https://jira.dev.pardot.com/browse/" + cce + "' target='_blank'>" + cce + "</a><div>");
             // cache the cce of the current page
             tempCCE = cce;
         }
         
-        //  how I'm handling Desk tabs - place all PDTs into an array
+        //  place all PDTs into an array
         pdtArray = document.querySelectorAll('#ticket_custom3');
         pdt = pdtArray[pdtArray.length - 1].value;
         
-        // don't add a link to the page if we're still on the same page
-        if (pdt !== tempPDT) {
-            pdtArray[pdtArray.length - 1].insertAdjacentHTML('beforebegin', "<a href='https://jira.dev.pardot.com/browse/" + pdt + "' target='_blank'>" + pdt + "</a>");
+        // don't add a link to the page once it's been added
+        if (pdt !== tempPDT && !(badLinkCheck.test(pdt))) {
+            pdtArray[pdtArray.length - 1].parentElement.insertAdjacentHTML('afterend', "<div class='jiraLink'><img src='https://i.imgur.com/cXxqLDL.png'><a href='https://jira.dev.pardot.com/browse/" + pdt + "' target='_blank'>" + pdt + "</a></div>");
             // cache the pdt of the current page
             tempPDT = pdt;
         }
